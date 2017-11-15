4095cbbdbf9381b6d0bb6a34b00a0363b49d48c1
Larry Madeo <lmadeo76@gmail.com>
2017-11-15 10:07:29 +0000

    added db functionality to User & Review classes

--- models/review.py
+++ models/review.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python
 """ holds class Review"""
+
 from models.base_model import BaseModel
+from models.place import Place
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Review(BaseModel):
     """Representation of Review """
-    place_id = ""
-    user_id = ""
-    text = ""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey("places.id"))
+        user_id = Column(String(60), ForeignKey("users.id"))
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
 
     def __init__(self, *args, **kwargs):
         """initializes Review"""

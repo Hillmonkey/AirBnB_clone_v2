ec13f2c9aac2747b501ba0e619b2b6143aee08f6
Larry Madeo <lmadeo76@gmail.com>
2017-11-20 20:30:56 -0800

    script to serve default settings to 2 servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# this script sets up webservers for deployment of static website
+
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo service nginx start
+
+sudo mkdir --parents /data/web_static/releases/test
+sudo mkdir /data/web_static/shared
+echo "Testing, testing, testing" | sudo tee /data/web_static/releases/test/index.html
+# maybe ln --force will cause replacement of preexisting file
+sudo ln --force -s -T /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo rm /etc/nginx/site-available/default
+wget https://my_default
+sudo mv default /etc/nginx/sites-available/default
+sudo service nginx restart

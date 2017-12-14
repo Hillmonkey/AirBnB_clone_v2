fab4018355d2bea0f0902e797406293b440b577b
Larry Madeo <lmadeo76@gmail.com>
2017-12-14 06:26:08 +0000

    rename template #5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def number_test(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number_template.html', n=n)
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
